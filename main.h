bebc6daaa57530a878a49dfcdcc4852c42749ca4
KOUASSI Israel <kouassiisrael36@gmail.com>
2022-11-22 22:53:52 -0800

    files updated

--- main.h
+++ main.h
@@ -40,7 +40,7 @@ unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsigned_number(unsigned int);
+int print_unsgined_number(unsigned int);
 
 
 #endif
